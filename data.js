aafcd219d236811df9cce06a27e6dfd174a9914a
phungvu2012 <phungvuminh2012@gmail.com>
2020-06-30 20:05:59 +0700

    Tạo các thành phần cơ bản của Game #9

---
+++ data.js
@@ -0,0 +1,26 @@
+// var data = {
+// 	screen: {
+// 		width: window.innerWidth,
+// 		height: document.body.clientHeight;
+// 	},
+// 	backgroundImage: {
+// 		url: "views/background-color/color (10).png",
+// 		width: 2048,
+// 		height: 1536
+// 	},
+// 	kodiImage: {
+// 		url: "views/kodi/kodi (1).png",
+// 		width: 1560,
+// 		height: 408
+// 	},
+// 	flatform: {
+// 		url: "views/kodi/platform.png",
+// 		width: 208,
+// 		height: 180
+// 	},
+// 	video: {
+// 		url: "views/menu/settings_video_icn-ipad@2x.png",
+// 		width: 100,
+// 		height: 100
+// 	}
+// }
