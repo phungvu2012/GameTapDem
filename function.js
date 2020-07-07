84f5d58376fe64aee0db93f1adc9382eb85c9d7d
phungvu2012 <phungvuminh2012@gmail.com>
2020-07-07 09:12:41 +0700

    Các function chỉnh lề tạo số ngẫu nhiên #2

---
+++ function.js
@@ -0,0 +1,8 @@
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
+function setPaddingElement(element, top, right, bottom, left) {
+	element.style.padding = top + "px " + right + "px " + bottom + "px " + left + "px";
+}
